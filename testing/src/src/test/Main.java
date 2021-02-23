3cd74fd0c8c7fd855234c656222373c35842a10d
gerheinz <gerheinz@host.docker.internal>
2021-02-23 11:53:21 +0100

    First commit

---
+++ testing/src/src/test/Main.java
@@ -0,0 +1,11 @@
+package src.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+	}
+}
