ec8285cfaa756cc6060acdaea888352ca60bb780
germanheinz <heinz.german@gmail.com>
2021-02-23 19:30:46 +0100

    Update Main.java

--- testing/src/src/test/Main.java
+++ testing/src/src/test/Main.java
@@ -4,52 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("9");
-		System.out.println("8");
-		System.out.println("7");
-		
-
-		System.out.println("G");
-		System.out.println("G");
-		System.out.println("G");
-		
-	
-		
-		System.out.println("Z");
-		System.out.println("Z");
-		System.out.println("Z");
-		
-		System.out.println("local1");
-		System.out.println("local1");
-		System.out.println("local1");
-		
-		System.out.println("local2");
-		System.out.println("local2");
-		System.out.println("local2");
-
-		System.out.println("local3");
-		System.out.println("local3");
-		System.out.println("local3");
-		
-		System.out.println("local4");
-		System.out.println("local4");
-		System.out.println("local4");
-		
-		System.out.println("A");
-		System.out.println("B");
-		System.out.println("C");
-		
-		System.out.println("D");
-		System.out.println("E");
-		System.out.println("F");
-		
-		System.out.println("x1");
-		System.out.println("x2");
-		System.out.println("x3");
-		
-		System.out.println("z1");
-		System.out.println("z2");
-		System.out.println("z3");
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
 
 	}
 }
