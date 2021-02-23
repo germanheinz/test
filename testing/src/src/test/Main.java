5993a42e2dfdd71ce9eba7cd74dddb92b9ae2ea8
germanheinz <heinz.german@gmail.com>
2021-02-23 20:35:38 +0100

    lala
    lala

--- testing/src/src/test/Main.java
+++ testing/src/src/test/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("A");
-		System.out.println("B");
-		System.out.println("C");
+		System.out.println("0");
+		System.out.println("0);
+		System.out.println("0);
 	
 
 	}
