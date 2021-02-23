740252ecf45ed59d0b13d7d9b87cc6b35baea1cd
gerheinz <gerheinz@host.docker.internal>
2021-02-23 20:27:26 +0100

    CHANGE FOR ABC

--- testing/src/src/test/Main.java
+++ testing/src/src/test/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("1");
-		System.out.println("2");
-		System.out.println("3");
+		System.out.println("A");
+		System.out.println("B");
+		System.out.println("C");
 	
 
 	}
