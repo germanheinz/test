90efdf43cdfc6a323f86d7d53b5bd4e982ec4614
gerheinz <gerheinz@host.docker.internal>
2021-02-23 20:58:01 +0100

    tester 1

--- testing/src/src/test/Main.java
+++ testing/src/src/test/Main.java
@@ -4,25 +4,10 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("a");
-		System.out.println("b");
-		System.out.println("c");
-		
-		System.out.println("d");
-		System.out.println("e");
-		System.out.println("f");
-		
-		System.out.println("g");
-		System.out.println("h");
-		System.out.println("i");
-
+		System.out.println("1");
+		System.out.println("2");
 		System.out.println("3");
-		System.out.println("4");
-		System.out.println("5");
-		
-		System.out.println("6");
-		System.out.println("7");
-		System.out.println("8");
+	
 
 	}
 }
