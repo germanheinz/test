310944ccdcb553c2a72761a1a2ad38c97d6cf3be
germanheinz <heinz.german@gmail.com>
2021-03-11 12:28:28 +0100

    Bad Commit

--- testing/src/src/test/Main.java
+++ testing/src/src/test/Main.java
@@ -4,44 +4,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo");
-		
-		System.out.println("a");
-		System.out.println("b");
-		System.out.println("c");
-		System.out.println("d");
 
-		String variable = "Add a value here!";
-		System.out.println("f");
-		
-		System.out.println("f");
-		System.out.println("g");
-		System.out.println("h");
-		
-		String newGithub = "test";
-		System.out.println("newGithub" + newGithub);
-		
-		
-		String myChangeGithub = "mum";
-		System.out.println("myChangeGithub");
-		
-		String myImportantVariable = "efgh";
-		String password2 = "5678";
-		
-		String importantUser = "ger";
-		String password = "hei";
-		System.out.println("importantUser" + importantUser);
-		System.out.println("password" + password);
-		
-		System.out.println("ESTE ES MI MENSAJE DESDE LOCAL: myImportantVariable " + myChangeGithub);
-		System.out.println("ESTE ES MI MENSAJE DESDE LOCAL: password2 " + password2);
-		
-
-		String newImportantValues = "test";
-		System.out.println("LOCAL AQUI" + newImportantValues);
-		
-		System.out.println("GITHUB AQUII" + importantUser);
-		System.out.println("GITHUB AQUII" + importantUser);
 
 	}
 }
