45339f3be7d78b5083540b12c4fad44fd5743989
germanheinz <heinz.german@gmail.com>
2021-03-11 11:52:44 +0100

    from start

--- testing/src/src/test/Main.java
+++ testing/src/src/test/Main.java
@@ -18,46 +18,6 @@ public class Main {
 		
 		
 		String myChangeGithub = "mum";
-		System.out.println("mumumumumumumumu");
-		String myChangeGithub2 = "mum";
-		System.out.println("mumumumumumumumu");
-		String myChangeGithub3 = "mum";
-		String myChangeGithu = "LULU";
-		System.out.println("LULU");
-		String myChangeGithu2 = "LULU";
-		System.out.println("LULU");
-		String myChangeGithu3 = "LULU";
-		
-		String myChangeLocal = "mum";
-		String myChangeLoca = "LULU";
-
-		String myChangeLocal2 = "mum";
-		String myChangeLoca2 = "LULU";
-
-		String myChangeLocal3 = "mum";
-		String myChangeLoca3 = "LULU";
-
-		String myChangeLocal4 = "mum";
-		String myChangeLoca4 = "LULU";
-		
-		String myChangeLocal5 = " mum";
-		String myChangeLoca5 = "LULU";
-
-		System.out.println("mum");
-		System.out.println("LULU");
-		
-		String myChangeLocal6 = " mum";
-		String myChangeLoca6 = "LULU";
-		
-		System.out.println("mum");
-		System.out.println("LULU");
-		
-		System.out.println("mum");
-		System.out.println("LULU");
-		
-		System.out.println(myChangeGithub);
-		
-		String de = "new";
-		String d = "LULU";
+		System.out.println("myChangeGithub");
 	}
 }
