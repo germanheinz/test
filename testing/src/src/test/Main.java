06004dd64337240bc56dd1d9b5fa4e53979c3b3c
germanheinz <heinz.german@gmail.com>
2021-03-11 11:31:51 +0100

    LELE GITHUB

--- testing/src/src/test/Main.java
+++ testing/src/src/test/Main.java
@@ -16,22 +16,24 @@ public class Main {
 		System.out.println("h");
 		System.out.println("i");
 		
-		String myChangeGithub = "myChangeGithubLALA";
+		String myChangeGithub = "myChangeGithubLELE";
 
-		String myChangeGithub2 = "myChangeGithubLALA";
+		String myChangeGithub2 = "myChangeGithubLELE";
 
-		String myChangeGithub3 = "myChangeGithubLALA";
+		String myChangeGithub3 = "myChangeGithubLELE";
 		
-		String myChangeLocal = "myChangeLocalLALA";
+		String myChangeLocal = "myChangeLocalLELE";
 
-		String myChangeLocal2 = "myChangeLocalLALA";
+		String myChangeLocal2 = "myChangeLocalLELE";
 
-		String myChangeLocal3 = "myChangeLocalLALA";
+		String myChangeLocal3 = "LELE";
 
-		String myChangeLocal4 = "myChangeLocalLALA";
+		String myChangeLocal4 = "LELE";
 		
 		String myChangeLocal5 = " LI LI LI";
-
+		
+		System.out.println("LE LE LE");
+		
 		String myChangeLocal6 = " LI LI LI";
 		
 		System.out.println("lalala");
